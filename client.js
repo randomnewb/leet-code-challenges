8ae0b771a0325aefd16c93000d85f03f20586c75
randomnewb <nguy1581@gmail.com>
2023-02-27 14:31:15 -0600

    Rearranged exercises based on completion date

--- client.js
+++ client.js
@@ -1,6 +1,93 @@
 console.log("Hello world!");
 
 /* 
+Tribonacci Sequence
+
+https://www.codewars.com/kata/556deca17c58da83c00002db
+
+Well met with Fibonacci bigger brother, AKA Tribonacci.
+
+As the name may already reveal, it works basically like a Fibonacci, 
+but summing the last 3 (instead of 2) numbers of the sequence to generate the next. 
+And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+
+So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+
+[1, 1 ,1, 3, 5, 9, 17, 31, ...]
+But what if we started with [0, 0, 1] as a signature? 
+As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, 
+you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+
+[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+Well, you may have guessed it by now, but to be clear: 
+you need to create a fibonacci function that given a signature array/list, 
+returns the first n elements - signature included of the so seeded sequence.
+
+Signature will always contain 3 numbers; 
+n will always be a non-negative number; 
+if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+If you enjoyed this kata more advanced and generalized version of it can be found in the Xbonacci kata
+
+[Personal thanks to Professor Jim Fowler on Coursera for his awesome classes that I really recommend 
+to any math enthusiast and for showing me this mathematical curiosity too with his usual contagious passion :)]
+
+Completed on January 25th, 2023
+
+function tribonacci(signature, n) {
+  // 0 case
+  if (n == 0) {
+    return [];
+  }
+
+  // single case
+  if (n == 1) {
+    let single_signature = [];
+    single_signature.push(signature[0]);
+
+    return single_signature;
+  }
+
+  // double case
+  if (n == 2) {
+    let double_signature = [];
+    double_signature.push(signature[0]);
+    double_signature.push(signature[1]);
+
+    return double_signature;
+  }
+
+  let next = 0;
+  let j = 0;
+
+  // -3 because of triple fibonacci sequence
+  while (j < n - 3) {
+    for (let i = j; i < signature.length; i++) {
+      next += signature[i];
+    }
+
+    signature.push(next);
+    j++;
+    next = 0;
+  }
+
+  return signature;
+}
+
+const a = [1, 1, 1];
+const b = 10;
+const c = [3, 2, 1];
+const d = 10;
+const e = [300, 200, 100];
+const f = 0;
+
+console.log(tribonacci(a, b));
+console.log(tribonacci(c, d));
+console.log(tribonacci(e, f));
+
+**/
+
+/* 
 Isograms
 
 https://www.codewars.com/kata/54ba84be607a92aa900000f1
@@ -174,89 +261,3 @@ console.log(highAndLow(a));
 console.log(highAndLow(b));
 console.log(highAndLow(c));
 **/
-
-/* 
-Tribonacci Sequence
-
-https://www.codewars.com/kata/556deca17c58da83c00002db
-
-Well met with Fibonacci bigger brother, AKA Tribonacci.
-
-As the name may already reveal, it works basically like a Fibonacci, 
-but summing the last 3 (instead of 2) numbers of the sequence to generate the next. 
-And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
-
-So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
-
-[1, 1 ,1, 3, 5, 9, 17, 31, ...]
-But what if we started with [0, 0, 1] as a signature? 
-As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, 
-you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
-
-[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
-Well, you may have guessed it by now, but to be clear: 
-you need to create a fibonacci function that given a signature array/list, 
-returns the first n elements - signature included of the so seeded sequence.
-
-Signature will always contain 3 numbers; 
-n will always be a non-negative number; 
-if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
-
-If you enjoyed this kata more advanced and generalized version of it can be found in the Xbonacci kata
-
-[Personal thanks to Professor Jim Fowler on Coursera for his awesome classes that I really recommend 
-to any math enthusiast and for showing me this mathematical curiosity too with his usual contagious passion :)]
-
-Completed on January 25th, 2023
-**/
-
-function tribonacci(signature, n) {
-  // 0 case
-  if (n == 0) {
-    return [];
-  }
-
-  // single case
-  if (n == 1) {
-    let single_signature = [];
-    single_signature.push(signature[0]);
-
-    return single_signature;
-  }
-
-  // double case
-  if (n == 2) {
-    let double_signature = [];
-    double_signature.push(signature[0]);
-    double_signature.push(signature[1]);
-
-    return double_signature;
-  }
-
-  let next = 0;
-  let j = 0;
-
-  // -3 because of triple fibonacci sequence
-  while (j < n - 3) {
-    for (let i = j; i < signature.length; i++) {
-      next += signature[i];
-    }
-
-    signature.push(next);
-    j++;
-    next = 0;
-  }
-
-  return signature;
-}
-
-const a = [1, 1, 1];
-const b = 10;
-const c = [3, 2, 1];
-const d = 10;
-const e = [300, 200, 100];
-const f = 0;
-
-console.log(tribonacci(a, b));
-console.log(tribonacci(c, d));
-console.log(tribonacci(e, f));
