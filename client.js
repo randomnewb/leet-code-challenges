32926bfa50569778db0f6d1a8a808ef7b198ac6c
randomnewb <nguy1581@gmail.com>
2023-02-27 13:48:28 -0600

    Completed Highest and Lowest (https://www.codewars.com/kata/554b4ac871d6813a03000035)

--- client.js
+++ client.js
@@ -87,7 +87,6 @@ Should return: 11 (the only odd number)
 Should return: 160 (the only even number)
 
 Completed on January 24th, 2023
-**/
 
 function findOutlier(integers) {
   let evenOrOdd = 0;
@@ -128,3 +127,50 @@ const b = [160, 3, 1719, 19, 11, 13, -21];
 
 console.log(findOutlier(a));
 console.log(findOutlier(b));
+
+**/
+
+/*
+Highest and Lowest
+
+https://www.codewars.com/kata/554b4ac871d6813a03000035
+
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+Notes
+-   All numbers are valid Int32, no need to validate them.
+-   There will always be at least one number in the input string.
+-   Output string must be two numbers separated by a single space, and highest number is first.
+
+Completed on January 25th, 2023
+**/
+
+function highAndLow(numbers) {
+  let split = numbers.split(" ");
+
+  let parsed = [];
+
+  for (let i = 0; i < split.length; i++) {
+    let convert = parseInt(split[i]);
+    parsed.push(convert);
+  }
+
+  let max = Math.max(...parsed).toString();
+  let min = Math.min(...parsed).toString();
+  let max_min = max + " " + min;
+
+  return max_min;
+}
+
+const a = "1 2 3 4 5";
+const b = "1 2 -3 4 5";
+const c = "1 9 3 4 -5";
+
+console.log(highAndLow(a));
+console.log(highAndLow(b));
+console.log(highAndLow(c));
